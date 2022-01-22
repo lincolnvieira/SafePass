5a4deb3a51a05feb034936100b59f10a10345aa1
Lincoln Vieira <lincoln_vieira@outlook.com.br>
2022-01-22 20:40:46 -0300

    Estrutura inicial do projeto

---
+++ SafePass/SafePass.WebApp/DataAccess/Dapper.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace SafePass.WebApp
+{
+    public class Dapper
+    {
+        private static readonly string _conexao = ConfigurationManager.AppSettings["CONNECTIONSTRING"];
+
+        public Dapper()
+        {
+            
+        }
+
+        public int Execute(string query, object parameters, CommandType commandType)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_conexao))
+                {
+                    return conn.Execute(query, parameters, commandType: commandType);
+                }
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+        }
+
+        public int Execute<T>(string query, object parameters, CommandType commandType)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_conexao))
+                {
+                    return conn.Execute(query, parameters, commandType: commandType);
+                }
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+        }
+
+        public DataTable ExecuteReader(string query, object parameters, CommandType commandType)
+        {
+            DataTable dttResult = new DataTable();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_conexao))
+                {
+                    var reader = conn.ExecuteReader(query, parameters, commandType: commandType);
+
+                    dttResult.Load(reader);
+
+                    return dttResult;
+                }
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
+        }
+
+        public object ExecuteScalar(string query, object parameters, CommandType commandType)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_conexao))
+                {
+                    return conn.ExecuteScalar(query, parameters, commandType: commandType);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return default;
+            }
+        }
+
+        public T QueryFirst<T>(string query, object parameters, CommandType commandType)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_conexao))
+                {
+                    return conn.QueryFirst<T>(query, parameters, commandType: commandType);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return default;
+            }
+        }
+
+        public T QuerySingle<T>(string query, object parameters, CommandType commandType)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_conexao))
+                {
+                    return conn.QuerySingle<T>(query, parameters, commandType: commandType);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return default;
+            }
+        }
+
+        public DataTable QuerySingleOrDefault(string query, object parameters, CommandType commandType)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(_conexao))
+                {
+                    return conn.QuerySingleOrDefault(query, parameters, commandType: commandType);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return default;
+            }
+        }
+    }
+}
